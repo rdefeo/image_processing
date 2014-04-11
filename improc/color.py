607427588711a15d398481746a8984d36040b221
Rob De Feo <robertodefeo@hotmail.com>
2014-04-11 17:40:13 +0800

    get background color (simple) closes #42

--- improc/color.py
+++ improc/color.py
@@ -3,3 +3,11 @@ import numpy as np
 
 def IsWhite(pixel):
   return pixel[0] == 255 and pixel[1] == 255 and pixel[2] == 255
+  
+def Background(img):
+  if img[0][0][0] == img[len(img)-1][0][0] == img[len(img)-1][len(img[0])-1][0] == img[0][len(img[0])-1][0] \
+    and img[0][0][1] == img[len(img)-1][0][1] == img[len(img)-1][len(img[0])-1][1] == img[0][len(img[0])-1][1] \
+    and img[0][0][2] == img[len(img)-1][0][2] == img[len(img)-1][len(img[0])-1][2] == img[0][len(img[0])-1][2]:
+    return img[0][0]
+  else:
+    return None
--- improc/tests/test_color.py
+++ improc/tests/test_color.py
@@ -1,5 +1,5 @@
 import unittest
-from improc.color import IsWhite
+from improc.color import IsWhite, Background
 import cv2
 import numpy as np
 
@@ -9,6 +9,19 @@ class IsWhiteTests(unittest.TestCase):
   
   def test_non_white(self):
     self.assertEqual(IsWhite(np.array( [156,34,125] )), False)
+
+class BackgroundTests(unittest.TestCase):
+  def test_white(self):
+    img = cv2.imread('improc/data/white_background_data.jpg')
+    color = Background(img)
+    self.assertEqual(color[0], 255)
+    self.assertEqual(color[1], 255)
+    self.assertEqual(color[2], 255)
+
+  def test_non_white(self):
+    img = cv2.imread('improc/data/non_white_background_data.jpg')
+    color = Background(img)
+    self.assertEqual(color, None)
 
 if __name__ == '__main__':
   unittest.main()
