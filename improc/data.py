31e89f9f3991eb6e5cb7c0695ca0be7fcaf05ae6
Rob De Feo <robertodefeo@hotmail.com>
2014-05-10 16:19:56 +0800

    closes #54

--- improc/data.py
+++ improc/data.py
@@ -5,18 +5,34 @@ def DataExtractor(object):
   port = None
   db = None
   collection = None
-  query = {}
+  query_data = {}
+  limit = None
+
   def connect(self):
     connection = pymongo.Connection(self.server, self.port)
     db = connection[self.db
     self.collection = db[self.collection]
 
-  def __init__(self, C = 1, gamma = 0.5):
-    pass
+  # def __init__(self, C = 1, gamma = 0.5):
+  #   pass
+
+  def query(self):
+    self.build()
+    docs = None
+
+    docs = db.shoes.find(
+      query_data,
+      # {
+      #   "shoe": 1
+      # }
+    ).limit(self.limit)
+
+    return docs
+
 
 def GetterExtractor(DataExtractor):
   query_header = {}
-  
+
   def __init__(self, server = MONGODB_GETTER_SERVER, port = MONGODB_GETTER_PORT, db = MONGODB_GETTER_DB, collection = MONGODB_GETTER_COLLECTION):
     self.server = server
     self.port = port
@@ -25,7 +41,7 @@ def GetterExtractor(DataExtractor):
     self.xconnect()
 
   def build(self):
-    query = {}
+    query_data = {}
 
     if len(query_header.keys())
-      query["header"] = query_header
+      query_data["header"] = query_header
