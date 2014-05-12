5135b927c8450a29631a1ad3e137c4b1710ca2e5
Rob De Feo <robertodefeo@hotmail.com>
2014-05-12 17:58:55 +0800

    closes #66

--- improc/features/comparator.py
+++ improc/features/comparator.py
@@ -1,6 +1,7 @@
 import mahotas.features
 import cv2
 import numpy as np
+import scipy.spatial.distance
 
 class DistanceComparator(object):
   name = None
@@ -17,5 +18,14 @@ class ChiSquaredComparator(DistanceComparator):
     d = 0.5 * np.sum([((a - b) ** 2) / (a + b + self.eps)
         for (a, b) in zip(sampleA, sampleB)])
 
-    # return the chi-squared distance
+    return d
+
+class EuclideanComparator(DistanceComparator):
+  def __init__(self):
+    # store the number of bins the histogram will use
+    self.name = "Euclidean"
+
+  def compare(self, sampleA, sampleB):
+    d = scipy.spatial.distance.euclidean(sampleA, sampleB)
+
     return d
