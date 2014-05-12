f39d66ac0a5b470073e46706b9a3134519626fea
Rob De Feo <robertodefeo@hotmail.com>
2014-05-12 17:52:48 +0800

    closes #66

--- improc/features/comparator.py
+++ improc/features/comparator.py
@@ -3,13 +3,18 @@ import cv2
 import numpy as np
 
 class DistanceComparator(object):
-  pass
+  name = None
 
 
 class ChiSquaredComparator(DistanceComparator):
-  def compare(self, sampleA, sampleB, eps = 1e-10):
+  def __init__(self, eps = 1e-10):
+    # store the number of bins the histogram will use
+    self.name = "ChiSquared"
+    self.eps = eps
+
+  def compare(self, sampleA, sampleB):
     # compute the chi-squared distance
-    d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
+    d = 0.5 * np.sum([((a - b) ** 2) / (a + b + self.eps)
         for (a, b) in zip(sampleA, sampleB)])
 
     # return the chi-squared distance
---
+++ improc/features/query.py
@@ -0,0 +1,32 @@
+def do(sample, possibles, comparator):
+  """This function does something.
+
+  Args:
+     sample (numpy.ndarray):  The item to use as the query
+     possibles (dict<numpy.ndarray>):  The item to use as the query
+     comparator (DistanceComparator): The comparator to use
+
+  Kwargs:
+     state (bool): Current state to be in.
+
+  Returns:
+
+
+  Raises:
+
+  """
+  results = []
+  for x in possibles.keys():
+    distance = comparator.compare(sample, possibles[x])
+    res = {
+      "value": distance,
+      "key": x
+    }
+    results.append(res)
+
+  sorted_results = sorted(results, key=lambda x: x["value"])
+
+  return {
+    "comparator": comparator.name,
+    "results": sorted_results
+  }
