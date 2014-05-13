a79b6400a30f51e6559bf81e3f0f1212bbf2bc9a
Rob De Feo <robertodefeo@hotmail.com>
2014-05-13 15:24:41 +0800

    closes #56

--- improc/features/descriptor.py
+++ improc/features/descriptor.py
@@ -1,19 +1,19 @@
 import mahotas.features
 import cv2
 
-class FeatureExtractor(object):
+class FeatureDescriptor(object):
   pass
 
-class HarlickExtractor(FeatureExtractor):
-  def extract(self, img):
+class HarlickDescriptor(FeatureDescriptor):
+  def describe(self, img):
     return mahotas.features.haralick(img).mean(0)
 
-class RgbHistogramExtrator(FeatureExtractor):
+class RgbHistogramDescriptor(FeatureDescriptor):
   def __init__(self, bins = [8, 8, 8]):
 		# store the number of bins the histogram will use
 		self.bins = bins
 
-  def extract(self, img):
+  def describe(self, img):
 		# compute a 3D histogram in the RGB colorspace,
 		# then normalize the histogram so that images
 		# with the same content, but either scaled larger
@@ -24,3 +24,16 @@ class RgbHistogramExtrator(FeatureExtractor):
 
 		# return out 3D histogram as a flattened array
 		return hist.flatten()
+
+class ZernikeDescriptor(FeatureDescriptor):
+  def __init__(self, radius = 21):
+    self.radius = radius
+
+  def describe(self, img):
+    processed_image = None
+    if len(img.shape) == 3:
+      processed_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+      processed_image = img
+
+    return mahotas.features.zernike_moments(processed_image, self.radius)
